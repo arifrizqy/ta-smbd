1c19feb24fa0ec2d1695b1222a5d494e15ec4224
arifrizqy <m.arifrizqy.f@gmail.com>
2024-06-13 04:05:05 +0700

    entah ngapain

--- pages/rolesnprevileges/script.js
+++ pages/rolesnprevileges/script.js
@@ -1,6 +1,25 @@
 $(document).ready(function () {
    $("#dataRolesNPrevileges").DataTable();
-   $('.tree-label[data-toggle="collapse"]').on("click", function () {
-      $(this).find(".tree-toggler").toggleClass("collapsed");
+
+   $('[data-toggle="collapse"]').on("click", function () {
+      var icon = $(this).find(".tree-toggler .fas");
+      if ($(this).attr("aria-expanded") === "true") {
+         icon.removeClass("fa-angle-down").addClass("fa-angle-right");
+      } else {
+         icon.removeClass("fa-angle-right").addClass("fa-angle-down");
+      }
+   });
+
+   $(".tree-check").on("change", function () {
+      var target = $(this).data("target");
+      $("#" + target)
+         .find('input[type="checkbox"]')
+         .prop("checked", $(this).is(":checked"));
+   });
+
+   $(".child-check").on("change", function () {
+      var parent = $(this).data("parent");
+      var allChecked = $("#" + parent + ' input[type="checkbox"]').length === $("#" + parent + ' input[type="checkbox"]:checked').length;
+      $('.tree-check[data-target="#' + parent + '"]').prop("checked", allChecked);
    });
 });
